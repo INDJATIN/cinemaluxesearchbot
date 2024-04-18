84f785f61b509d27f32218871af123b9a480c4d1
INDJATIN <130268528+INDJATIN@users.noreply.github.com>
2024-04-18 09:26:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', '27663778'))
-API_HASH = environ.get('API_HASH', '2e751726850d421b67021e0edf85cbc6')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7192994294:AAEZnKANYuUX7z1QI872TqFnmLDHpqKM4Yw')
-ADMIN = environ.get('ADMIN', '1164156857') #seperate by id # Replace this with your group chat ID\
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
+ADMIN = environ.get('ADMIN', '') #seperate by id # Replace this with your group chat ID\
 
-DOMAIN = environ.get('DOMAIN', 'https://cinemaluxe.autos/?s=') 
+DOMAIN = environ.get('DOMAIN', '') 
